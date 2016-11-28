07150b27cbcf266799a975d34bb70e9c3b9ba0b7
Boris <Boris>
2016-11-28 22:34:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/boris/sm_gsm/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.boris.sm_gsm;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.mingle.entity.MenuEntity;
+import com.mingle.sweetpick.DimEffect;
+import com.mingle.sweetpick.RecyclerViewDelegate;
+import com.mingle.sweetpick.SweetSheet;
+import com.mingle.sweetpick.ViewPagerDelegate;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SweetSheet mSweetSheet;
+    private RelativeLayout Rlayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Rlayout = (RelativeLayout) findViewById(R.id.Rlayout);
+
+        setupViewpager();
+
+    }
+
+    private void setupRE(){}
+
+
+    private void setupViewpager() {
+
+
+        mSweetSheet = new SweetSheet(Rlayout);
+
+        //настройка меню
+        mSweetSheet.setMenuList(R.menu.menu_sweet);
+        mSweetSheet.setDelegate(new RecyclerViewDelegate(true));
+        mSweetSheet.setBackgroundEffect(new DimEffect(0.6f));
+        mSweetSheet.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+            @Override
+            public boolean onItemClick(int position, MenuEntity menuEntity1) {
+
+                Toast.makeText(MainActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_secView) {
+            mSweetSheet.toggle();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
